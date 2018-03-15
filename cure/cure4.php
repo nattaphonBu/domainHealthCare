05422b430903f7f29d957e2f4c284b7a8c3fc264
Nattaphon_EnterHelloWorld <natlove.natlove202@gmail.com>
2018-03-15 22:56:56 +0700

    2

---
+++ cure/cure4.php
@@ -0,0 +1,47 @@
+
+<html>
+<?php
+   $content = 'This is about us page content';
+   include('../header2.php');
+?>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
+<link rel="stylesheet" href="https://v40.pingendo.com/assets/4.0.0/default/theme.css" type="text/css"> </head>
+<body>
+<div class="py-5">
+    <div class="container">
+      <div class="row mb-5">
+        <div class="col-md-7">
+          <h2 class="text-primary">กล้ามเนื้อฉีกขาด (muscular strain)</h2><br>
+          <h5 class="">สาเหตุ
+          : จากแรงกระแทกภายนอก   จากตัวกล้ามเนื้อเอง<br>
+          แบ่งเป็น 3 ระดับ
+    <p>ระดับ 1 ฉีกขาดน้อยกว่า 10 % บวมและปวดไม่มาก วิ่งต่อไปได้</p>
+    <p>ระดับ 2 ฉีกขาด 10-15 % บวม ปวดมาก วิ่งไม่ได้ พอเดินได้</p>
+    <p>ระดับ 3 ฉีกขาด 50-100 % บวมมาก ถ้าฉีกขาดสมบูรณ์ เล่นกีฬาหรือเดินไม่ได้</p>
+
+            </h5><br>
+            <h5 class="">การรักษา 
+            หยุดพักทันที ประคบเย็น 15-20 นาที  จากนั้นพันผ้ายืด ยกส่วนปลายสูง  1-2 วัน ให้ประคบร้อน
+            <p>ระดับ 3 ฉีกขาด 50-100 % บวมมาก ถ้าฉีกขาดสมบูรณ์ เล่นกีฬาหรือเดินไม่ได้</p>
+            <p>ระดับ 3 ฉีกขาด 50-100 % บวมมาก ถ้าฉีกขาดสมบูรณ์ เล่นกีฬาหรือเดินไม่ได้</p>
+            <p>ระดับ 3 ฉีกขาด 50-100 % บวมมาก ถ้าฉีกขาดสมบูรณ์ เล่นกีฬาหรือเดินไม่ได้</p>
+
+              
+            </h5><br>
+
+            
+            <a class="btn btn-primary" href="../findcure.php">ย้อนกลับ </a>
+          <a class="btn btn-primary" href="../index.php">กลับสู่หน้าหลัก </a>
+        </div>
+        
+    </div>
+</div>
+
+</body>
+<?php
+ include('../footer.php');
+?>
+</html>
---
+++ cure/cure5.php
@@ -0,0 +1,38 @@
+
+<html>
+<?php
+   $content = 'This is about us page content';
+   include('../header2.php');
+?>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
+<link rel="stylesheet" href="https://v40.pingendo.com/assets/4.0.0/default/theme.css" type="text/css"> </head>
+<body>
+<div class="py-5">
+    <div class="container">
+      <div class="row mb-5">
+        <div class="col-md-7">
+          <h2 class="text-primary">กล้ามเนื้อบวม (muscle swelling)</h2><br>
+          <h5 class="">สาเหตุ
+          : เลือดไปเลี้ยงกล้ามเนื้อมัดนั้นน้อย พบในนักวิ่งที่เริ่มฝึกซ้อมหนักเกินไป มักพบในกล้ามเนื้อขา
+            </h5><br>
+            <h5 class="">การรักษา 
+            หยุดออกกำลังทันที พักผ่อนวางอวัยวะส่วนที่ปวดในแนวราบ ถ้าไม่ดีขึ้นต้องได้รับการผ่าตัด
+              
+            </h5><br>
+
+            
+            <a class="btn btn-primary" href="../findcure.php">ย้อนกลับ </a>
+          <a class="btn btn-primary" href="../index.php">กลับสู่หน้าหลัก </a>
+        </div>
+        
+    </div>
+</div>
+
+</body>
+<?php
+ include('../footer.php');
+?>
+</html>
