a36e9057c8b600be8fbcd01d44ec03de6c786f28
Nattaphon_EnterHelloWorld <natlove.natlove202@gmail.com>
2018-03-15 23:57:28 +0700

    5

--- findcure.php
+++ findcure.php
@@ -28,6 +28,8 @@
               <div class="dropdown-divider"></div>
               <a class="dropdown-item" href="../../cure/cure5.php">ข้อเคล็ด ข้อแพลง (Sprain)</a>
               <div class="dropdown-divider"></div>
+              <br>
+              <a class="btn btn-primary" href="../index.php">กลับสู่หน้าหลัก </a>
             </div>
           </div>
         </div>
